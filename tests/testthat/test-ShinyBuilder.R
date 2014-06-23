26e4b080b498c3f5089aa4df56be57b759594773
mul118 <MikhailLisovich@clearchannel.com>
2014-06-23 14:58:13 -0400

    Test for db connections + sample queries

--- tests/testthat/test-ShinyBuilder.R
+++ tests/testthat/test-ShinyBuilder.R
@@ -2,4 +2,12 @@ context("ShinyBuilder")
 
 test_that('package functions are present',{
   expect_true(is.function(dbListInit))
+  expect_true(is.function(dbListAdd))
+  expect_true(is.function(dbListRemove))
+  expect_true(is.function(dbListPrint))
+})
+
+test_that('DB connections work with sample queries',{
+  db_list <- dbListInit()
+  lapply(db_list, function(db_obj) expect_is(with(db_obj, do.call(query_fn, list(db, default_query))), 'data.frame'))
 })
