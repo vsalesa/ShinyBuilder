d6004da8fc812e3b2ccdb4fbeadc8a34895f6b54
mul118 <MikhailLisovich@clearchannel.com>
2014-06-24 13:31:52 -0400

    Add auto-update functionality

--- R/ShinyBuilder.R
+++ R/ShinyBuilder.R
@@ -14,13 +14,30 @@ runShinyBuilder <- function(){
 #' Note that regardless of its deploy location, the app will use the ShinyBuilder library
 #' location for saving/loading dashboards, databases, and config files.
 #' @param dir directory to which ShinyBuilder app is deployed
+#' @param update logical value indicating whether ShinyBuilder dashboards should be periodically updated by re-running the queries.  True by default.
+#' @param times vector specifying update schedule in crontab format (i.e., minute (0-59), hour (0-23), day of month (1-31), month (1-12), day of week (0-6, Sun = 6)).  Defaults to daily updates at 12:00 AM.
 #' @examples
-#' debployShinyBuilder(dir = '/srv/shiny-server/ShinyBuilder') 
+#' deployShinyBuilder(dir = '/srv/shiny-server/ShinyBuilder') 
 #' @export
-deployShinyBuilder <- function(dir){
+deployShinyBuilder <- function(dir, update = T, times = c(0, 0, '*', '*', '*')){  
+    #Copy files
+    if(!file.exists(dir)) dir.create(dir, mode = '0755')
+    if(substr(dir, nchar(dir), nchar(dir)) != '/') dir <- paste0(dir, '/')
     sb_dir <- system.file('', package = 'ShinyBuilder')
-    if(substr(dir, nchar(dir), nchar(dir)) != '/')
-      dir <- paste0(dir, '/')
+    file.copy(from = paste0(sb_dir, c('server.R', 'ui.R', 'global.R')), to = paste0(dir, c('server.R', 'ui.R', 'global.R')), overwrite = T)
     
-    file.copy(from = paste0(sb_dir, c('server.R', 'ui.R', 'global.R')), to = paste0(dir, c('server.R', 'ui.R', 'global.R')))
+    #Crontabs
+    cron_script   <- "Rscript -e 'ShinyBuilder::updateDashboards()'"
+    crontabs      <- try(system('crontab -l', intern = T), silent = T)
+    script_index  <- grep(cron_script, crontabs, fixed = T)
+    if(length(script_index) == 0) script_index <- length(crontabs) + 1
+    ifelse(update,
+           crontabs[script_index] <- paste(paste(times, collapse = ' '), cron_script),
+           crontabs <- crontabs[-script_index])
+    
+    #Write out crontabs
+    write.table(crontabs, '/tmp/sb_crontabs.csv', row.names = F, col.names = F, quote = F)
+    system('crontab < /tmp/sb_crontabs.csv')
+    
+    message(paste0('ShinyBuilder deployed successfully. Autoupdate: ', ifelse(update, 'ON', 'OFF')))
 }
