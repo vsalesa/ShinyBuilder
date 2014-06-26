b438de0424c2a30b42eee7c796fcef9deacbb6e0
mul118 <MikhailLisovich@clearchannel.com>
2014-06-26 12:27:24 -0400

    Reactivate DB test

--- tests/testthat/test-ShinyBuilder.R
+++ tests/testthat/test-ShinyBuilder.R
@@ -7,7 +7,7 @@ test_that('package functions are present',{
   expect_true(is.function(dbListPrint))
 })
 
-# test_that('DB connections work with sample queries',{
-#   db_list <- dbListInit()
-#   lapply(db_list, function(db_obj) expect_is(with(db_obj, do.call(query_fn, list(db, default_query))), 'data.frame'))
-# })
+test_that('DB connections work with sample queries',{
+  db_list <- dbListInit()
+  lapply(db_list, function(db_obj) expect_is(with(db_obj, do.call(query_fn, list(db, default_query))), 'data.frame'))
+})
