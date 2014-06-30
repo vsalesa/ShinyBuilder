95ed867c7a2540e23f5db081e8d6b95d2a61cf85
mul118 <MikhailLisovich@clearchannel.com>
2014-06-30 13:51:55 -0400

    Fix permission setting for updateDashboards

--- R/updateDashboards.R
+++ R/updateDashboards.R
@@ -7,15 +7,23 @@
 #' @param dashboards a vector of dashboard names.  By default, all dashboards in the dashboards directory are updated
 #' @export 
 #' @examples
-#' \dontrun{updateDashboards()}
+#' \dontrun{
+#' #All Dashboards
+#' updateDashboards()
+#' #Selected dashboards
+#' updeateDashboards(c('dashboard_1', 'dashboard_2'))
+#' }
 updateDashboards <- function(dashboards = NULL){
+
+  sbd_path <- system.file('dashboards', package = 'ShinyBuilder')
+  
+  #Check/set permissions
+  Sys.chmod(sbd_path, mode = "0755")
   
   if(is.null(dashboards)) 
-    #dashboards <- list.files(path = paste0(getwd(),'/dashboards'), full.names = T)
-    dashboards <- list.files(path = system.file('dashboards', package = 'ShinyBuilder'), full.names = T)
+    dashboards <- list.files(path = sbd_path, full.names = T)
   
   db_list <- dbListInit()
-  #source('ShinyBuilder/R/db_list.R')
 
   for (dashboard_file in dashboards){
     #Load current dashboard
@@ -24,10 +32,10 @@ updateDashboards <- function(dashboards = NULL){
     
     #Update chart data
     for (i in 1:length(dashboard_state)){
-      if(str_detect(dashboard_state[[i]]$id, 'gItemPlot')){
+      if(grepl('gItemPlot', dashboard_state[[i]]$id)){
         input_query               <- dashboard_state[[i]]$query
         db_obj                    <- db_list[[dashboard_state[[i]]$db_name]]
-        dashboard_state[[i]]$data <- do.call(db_obj$qry_fn, list(db_obj$db, input_query))
+        dashboard_state[[i]]$data <- do.call(db_obj$query_fn, list(db_obj$db, input_query))
       }
     }
     #Save current dashboard
